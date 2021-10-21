0ff80f1145e717d8f71e407acb237999e0bb2416
Zdravoles <zdravoles@gmail.com>
2021-10-21 19:27:01 +0300

    Отрисуй меня полностью (часть 2)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {createOffer} from './create-offer.js';
 import {getOfferMarkup} from './get-offers.js';
+import {statusPageOn, statusPageOff} from './status-page.js';
 
 const OFFER_COUNT = 10;
 const OFFERS = Array.from({length: OFFER_COUNT}, createOffer);
@@ -7,3 +8,5 @@ const OFFERS = Array.from({length: OFFER_COUNT}, createOffer);
 const mapArea = document.querySelector('#map-canvas');
 const cardTemplate = document.querySelector('#card').content;
 getOfferMarkup(mapArea,cardTemplate,OFFERS,1);
+statusPageOff();
+statusPageOn();
---
+++ js/status-page.js
@@ -0,0 +1,37 @@
+const statusPageOff = () => {
+  const formAd = document.querySelector('.ad-form');
+  formAd.classList.add('ad-form--disabled');
+
+  const fieldsets = formAd.querySelectorAll('fieldset');
+  for (const element of fieldsets) {
+    element.disabled = 'disabled';
+  }
+
+  const formFilters = document.querySelector('.map__filters');
+  formFilters.classList.add('map__filters--disabled');
+
+  const filterElements = formFilters.children;
+  for (const element of filterElements) {
+    element.disabled = 'disabled';
+  }
+};
+
+const statusPageOn = () => {
+  const formAd = document.querySelector('.ad-form');
+  formAd.classList.remove('ad-form--disabled');
+
+  const fieldsets = formAd.querySelectorAll('fieldset');
+  for (const element of fieldsets) {
+    element.disabled = '';
+  }
+
+  const formFilters = document.querySelector('.map__filters');
+  formFilters.classList.remove('map__filters--disabled');
+
+  const filterElements = formFilters.children;
+  for (const element of filterElements) {
+    element.disabled = '';
+  }
+};
+
+export {statusPageOn, statusPageOff};
